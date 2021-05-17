b6f2dc5ae4e94db5afc0b4959004f14c99b4bf2f
dhararya <aryan.dhar014@gmail.com>
2021-05-17 12:35:46 -0400

    Some more styling

--- components/nameDescription.tsx
+++ components/nameDescription.tsx
@@ -5,8 +5,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
   root: {
     width: '100%',
-    maxWidth: 500,
     color: "#710193",
+    padding: "2rem 2rem",
   },
 });
 
@@ -16,10 +16,10 @@ export default function NameDescription() {
   return (
     <div className={classes.root}>
       <Typography variant="h2" component="h2" gutterBottom>
-        Musical GIF
+        <b>Musical GIF</b>
       </Typography>
       <Typography variant="body1" gutterBottom>
-        Musical GIF is service that takes in a custom caption, and returns an appropriate GIF and Spotify song to play with it. Below, is a custom-made version for the caption "Musical GIF will not give up on your GIF needs". Try it yourself now!
+        Musical GIF is service that takes in a custom caption, and returns an appropriate GIF and Spotify song to play with it. Below, is a custom-made version for the caption "Musical GIF will not give up on your musical GIF needs". Try it yourself now!
       </Typography>
     </div>
   );
